d3e0f0a70e4786403a7d28a396af7d8c0cf1e847
bachc <bachc@purdue.edu>
2015-09-16 08:38:40 -0500

    modified to be compatible with python 3. added back multi-language capability.

--- hello_world.py
+++ hello_world.py
@@ -4,15 +4,25 @@ That can be executed in both, python 2.x and 3.x
 """
 import sys
 
-#Language specific strings
-h_world = "Hello world!"
-p_key = "Press Enter to close this program"
+#Select language
+#L = 'English'
+#L= 'German'
 
+
+
+#Language specific string
+EN = {'h_world':  "Hello world!",
+'p_key': "Press Enter to close this program"}
+DE = {'h_world':  "Hallo Welt!",
+'p_key': "Druecke Enter um das Programm zu schliessen"}
+words = {'English': EN,
+         'German': DE}
+"""
 if sys.version_info[0]< 3:
-    print h_world
-else:
-    print(h_world)
+    print words[L]['h_world']
+else:"""
+#print(words[L]['h_world'])
 
-raw_input(p_key) 
-
-
+L = input('Select language ')
+print(words[L]['h_world'])
+input(words[L]['p_key'])
