4423d8e5dc66dbc305e85e745ee4daae66a785c3
bachc <bachc@purdue.edu>
2015-09-16 10:53:41 -0500

    added Swabian German and cleaned up file

--- hello_world.py
+++ hello_world.py
@@ -2,27 +2,19 @@
 This is a simple python hello world script,
 That can be executed in both, python 2.x and 3.x
 """
-import sys
-
-#Select language
-#L = 'English'
-#L= 'German'
-
-
 
 #Language specific string
 EN = {'h_world':  "Hello world!",
 'p_key': "Press Enter to close this program"}
 DE = {'h_world':  "Hallo Welt!",
 'p_key': "Druecke Enter um das Programm zu schliessen"}
-words = {'English': EN,
-         'German': DE}
-"""
-if sys.version_info[0]< 3:
-    print words[L]['h_world']
-else:"""
-#print(words[L]['h_world'])
+DE1 = {'h_world':  "Hallo Weld!",
+'p_key': "Hau uff'd Ender-Daschde druff ums Brogram z'schliesse!"}
+
+Language_dicts = {'English': EN,
+         'German': DE, 'Swabian German': DE1}
 
 L = input('Select language ')
-print(words[L]['h_world'])
-input(words[L]['p_key'])
+
+print(Language_dicts[L]['h_world'])
+input(Language_dicts[L]['p_key'])
