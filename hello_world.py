96eee423c5a6d9218c91122d20749bd6530ffaf8
Saad Saleem <saad.saleem@okstate.edu>
2019-02-23 12:55:44 -0600

    Added the strings for bengali language
    Signed-off-by:yeam

--- hello_world.py
+++ hello_world.py
@@ -24,11 +24,14 @@ SK = {'h_world':  "जगत नमस्कारः ",
 'p_key':  "Hit Enter to shut this program down"}
 URD = {'h_world': " السلام علیکم",
 'p_key':  "Hit Enter to shut this program down"}
-
+PUNJ = {'h_world': "Sati srī akāla duni'ā",
+'p_key':  "Hit Enter to shut this program down"}
+BEN = {'h_world': "ওহে বিশ্ব",
+'p_key':  "Hit Enter to shut this program down"}
 Language_dicts = {'English': EN,
          'Deutsch': DE, 'Swabian German': DE1, 'Espanol': ES, 
          'Chinese': CHN, 'Italian': ITA, 'Okie': OK, 'Sanskrit': SK,
-          'Urdu': URD}
+          'Urdu': URD,'Punjabi': PUNJ, 'BENGALI': BEN}
 
 print(Language_dicts.keys())
 
