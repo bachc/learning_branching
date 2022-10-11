f783fb6e587bd96682c3436e186464219c8e4922
TNWest <mastertigerwest@gmail.com>
2022-10-11 16:26:51 -0500

    Added Vietnamese as a supported language.

--- hello_world.py
+++ hello_world.py
@@ -10,10 +10,16 @@ DE = {'h_world':  "Hallo Welt!",
 'p_key': "Druecke Enter um das Programm zu schliessen"}
 DE1 = {'h_world':  "Hallo Weld!",
 'p_key': "Hau uff'd Ender-Daschde druff ums Brogram z'schliesse!"}
+vie = {'h_world': "Chào thế giới!",
+# Translation from EN using google translate:
+# https://translate.google.com/?sl=auto&tl=vi&text=Hello%20world&op=translate
+'p_key': "Nhấn Enter để đóng chương trình này"}
+# Translation from EN using google translate:
+# https://translate.google.com/?sl=auto&tl=vi&text=Press%20Enter%20to%20close%20this%20program&op=translate
 
 #Available language dicts       
 Language_dicts = {'English': EN,
-         'Deutsch': DE, 'Swabian German': DE1}
+         'Deutsch': DE, 'Swabian German': DE1, 'Vietnamese': vie}
 
 print("\nThe following languages are available:")
 for key in Language_dicts.keys():
