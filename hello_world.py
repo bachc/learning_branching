804152c5fab9ca9e237053239706a4cf1fe8d98d
Matin Ghadiri <mghadir@okstate.edu>
2022-10-13 21:31:42 -0500

    The necessary strings for my assigned language in the python file was added.

--- hello_world.py
+++ hello_world.py
@@ -4,31 +4,22 @@ That can be executed only in python 3.x.
 """
 
 #Language specific strings
-EN = {'h_world':  "Hello world!",
-'p_key': "Press Enter to close this program"}
-DE = {'h_world':  "Hallo Welt!",
-'p_key': "Druecke Enter um das Programm zu schliessen"}
-DE1 = {'h_world':  "Hallo Weld!",
-'p_key': "Hau uff'd Ender-Daschde druff ums Brogram z'schliesse!"}
-vie = {'h_world': "Chào thế giới!",
-# Translation from EN using google translate:
-# https://translate.google.com/?sl=auto&tl=vi&text=Hello%20world&op=translate
-'p_key': "Nhấn Enter để đóng chương trình này"}
-# Translation from EN using google translate:
-# https://translate.google.com/?sl=auto&tl=vi&text=Press%20Enter%20to%20close%20this%20program&op=translate
-Th={'h_world':  "สวัสดีชาวโลก!",
-      'p_key': "กด Enter เพื่อปิดโปรแกรมนี้"}
-zulu = {'h_world':  "Sawubona Mhlaba",
-      'p_key': "Cindezela u-Enter ukuze uvale lolu hlelo"}
-West_Frisian={'h_world': 'Hallo wrâld!',
-              'p_key': 'Druk op Enter om dit programma te sluten.'}
-yoruba = {'h_world':  "Mo ki O Ile Aiye!",
-'p_key': "Tẹ Tẹ lati pa eto yii"}
+EN = {'h_world':  "Hello world!", 'p_key': "Press Enter to close this program"}
+DE = {'h_world':  "Hallo Welt!", 'p_key': "Druecke Enter um das Programm zu schliessen"}
+DE1 = {'h_world':  "Hallo Weld!", 'p_key': "Hau uff'd Ender-Daschde druff ums Brogram z'schliesse!"}
+vie = {'h_world': "Chào thế giới!", 'p_key': "Nhấn Enter để đóng chương trình này"}
+Th={'h_world':  "สวัสดีชาวโลก!", 'p_key': "กด Enter เพื่อปิดโปรแกรมนี้"}
+zulu = {'h_world':  "Sawubona Mhlaba", 'p_key': "Cindezela u-Enter ukuze uvale lolu hlelo"}
+West_Frisian={'h_world': 'Hallo wrâld!', 'p_key': 'Druk op Enter om dit programma te sluten.'}
+yoruba = {'h_world':  "Mo ki O Ile Aiye!", 'p_key': "Tẹ Tẹ lati pa eto yii"}
+Tamil = {"h_world": "வணக்கம் உலகம்", "p_key": "இந்த நிரலை மூட Enter ஐ அழுத்தவும்"}
+
+
 
 #Available language dicts       
-Language_dicts = {'English': EN,
-         'Deutsch': DE, 'Swabian German': DE1, 'Vietnamese': vie, 'Thai': Th,
-         'zulu': zulu, 'Western Frisian': West_Frisian, 'Yoruba': yoruba}
+Language_dicts = {'English': EN, 'Deutsch': DE, 'Swabian German': DE1, 'Vietnamese': vie,
+                  'Thai': Th, 'zulu': zulu, 'Western Frisian': West_Frisian,
+                  'Yoruba': yoruba, 'Tamil': Tamil, 'tamil': Tamil, 'TAMIL': Tamil}
 
 print("\nThe following languages are available:")
 for key in Language_dicts.keys():
